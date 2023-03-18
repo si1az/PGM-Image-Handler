c81a34314e998d0c1d4a39d7533cacc7ecdbaed8
si1az <32607235+si1az@users.noreply.github.com>
2023-03-17 20:47:39 -0500

    Clarification upon reviewing

--- 7.1algorithm.cpp
+++ 7.1algorithm.cpp
@@ -1,7 +1,8 @@
+// This cpp file is non-functioning and was solely used in Notepad++ for brainstorming
 just subtract 255 from each item in the array and get the abs of it
 
 ar1[i][j] = abs(ar1[i][j]-255);
-or do abs(255-thearraypiece) //this is the function that is given
+or do abs(255-thearraypiece)
 
 avg of rows will have normal nested for loops
 
